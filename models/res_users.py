20256a4af4cfbe738dbf39707757a7e381f57333
MATHIEU <thibaud.mathieu@eridanis.com>
2019-07-31 10:29:42 +0200

    fix method not called on accessdenied error

--- models/res_users.py
+++ models/res_users.py
@@ -24,16 +24,16 @@ class Users(models.Model):
         user_id = super(Users, cls)._login(db, login, password)
         if user_id:
             return user_id
-        
+
         uid = validator.verify(password)
         _logger.info(uid)
 
         return uid
 
     @api.model
-    def _check_credentials(self, password):
+    def check_credentials(self, password):
         try:
-            super(Users, self)._check_credentials(password)
+            super(Users, self).check_credentials(password)
         except AccessDenied:
             # verify password as token
             if not validator.verify(password):
